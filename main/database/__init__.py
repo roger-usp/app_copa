273421636e3ac449c8ae5b8009a87a93c3c99831
Roger Bif <rbif03@usp.br>
2024-07-26 14:05:56 -0300

    package structure change

--- main/database/__init__.py
+++
@@ -1,4 +0,0 @@
-from .list_json import get_info_list
-from .polygon_geojson import make_poly_geojson
-from .arrow_geojson import make_arrow_geojson
-from .point_geojson import make_point_geojson
--- server/app.py
+++ server/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, jsonify, request
-import database as db
+from .database.list_json import get_info_list
+from .database.polygon_geojson import make_poly_geojson
+from .database.arrow_geojson import make_arrow_geojson
+from .database.point_geojson import make_point_geojson
 
 app = Flask(__name__)
 
@@ -9,15 +12,15 @@ def home():
 
 @app.route("/base-select-info")
 def base_select_info():
-    return jsonify(db.get_info_list("polygons/info"))
+    return jsonify(get_info_list("polygons/info"))
 
 @app.route("/point-select-info")
 def point_select_info():
-    return jsonify(db.get_info_list("points/info"))
+    return jsonify(get_info_list("points/info"))
 
 @app.route("/arrow-select-info")
 def arrow_select_info():
-    return jsonify(db.get_info_list("arrows/info"))
+    return jsonify(get_info_list("arrows/info"))
 
 @app.route("/polygon-geojson")
 def polygon_geojson():
@@ -25,7 +28,7 @@ def polygon_geojson():
     query = request.args.to_dict(flat=False)
     value = query["value"]
     info_file_name = value[0] + ".json"
-    poly_geojson = db.make_poly_geojson(info_file_name)
+    poly_geojson = make_poly_geojson(info_file_name)
     return jsonify(poly_geojson)
 
 @app.route("/arrow-geojson")
@@ -34,7 +37,7 @@ def arrow_geojson():
     query = request.args.to_dict(flat=False)
     values = query["value"]
     info_file_name_list = [val + ".json" for val in values]
-    arrow_geojson = db.make_arrow_geojson(info_file_name_list)
+    arrow_geojson = make_arrow_geojson(info_file_name_list)
     return jsonify(arrow_geojson)
 
 @app.route("/point-geojson")
@@ -43,11 +46,9 @@ def point_geojson():
     query = request.args.to_dict(flat=False)
     values = query["value"]
     info_file_name_list = [val + ".json" for val in values]
-    point_geojson = db.make_point_geojson(info_file_name_list)
+    point_geojson = make_point_geojson(info_file_name_list)
     return jsonify(point_geojson)
 
 @app.route("/data-sources")
 def data_sources():
     return render_template("data_sources.html")
-
-app.run(debug=True)
